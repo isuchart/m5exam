823964ef64608affbde48fefdd61fbaf0e54929a
isuchart <98205164+isuchart@users.noreply.github.com>
2024-02-17 21:06:38 +0700

    Update quizscript.js

--- quizscript.js
+++ quizscript.js
@@ -357,7 +357,7 @@ submitBtn.addEventListener("click", () => {
                         action: {
                           type: "uri",
                           label: "เริ่มทำแบบทดสอบใหม่",
-                          uri: "https://liff.line.me/1657810366-BnP29NW4",
+                          uri: "https://line.me/R/app/1657810366-BnP29NW4",
                         },
                         height: "sm",
                         style: "primary",
